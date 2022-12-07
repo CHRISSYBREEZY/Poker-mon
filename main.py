f0441d22de6bb62a9ada1c4652544dc91967434f
CHRISSYBREEZY <112854576+CHRISSYBREEZY@users.noreply.github.com>
2022-12-07 12:28:04 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import config
+from game_state import GameState
+import mysql.connector
+from game import Game
+#Importerer fra de forskjellige filene og databasen.
+
+pygame.init()
+
+player_locationDB = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="pokemon"
+)
+dbCusrsor = player_locationDB.cursor()
+
+screen = pygame.display.set_mode((600, 400))
+
+pygame.display.set_caption("Pokemon Clone")
+
+clock = pygame.time.Clock() 
+
+game = Game(screen)
+game.set_up()
+
+while game.game_state == GameState.RUNNING:
+    clock.tick(50)
+    game.update()
+    pygame.display.flip() 
